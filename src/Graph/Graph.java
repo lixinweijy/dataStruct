0c04ff98e14342cba6f41d8609095cc0f7d53a30
李新伟 <lixinweijy@gmail.com>
2022-10-07 12:17:20 +0800

    深度优先遍历

--- src/Graph/Graph.java
+++ src/Graph/Graph.java
@@ -9,6 +9,8 @@ public class Graph {
     private int[][] edges;//存储对应的邻结矩阵
     private int numOfEdges; //表示边的数目
 
+    //定义一个数组boolean[],记录某个节点是否被访问
+    private boolean[] isVisited;
     public static void main(String[] args) {
         //测试一把图是否创建OK
         int n=5;//结点的个数
@@ -29,6 +31,10 @@ public class Graph {
 
         //显示一把邻接矩阵
         graph.showGraph();
+
+        //测试一把
+        System.out.println("深度遍历");
+        graph.dfs();
     }
 
     //构造器
@@ -37,7 +43,56 @@ public class Graph {
         edges=new int[n][n];
         vertexList=new ArrayList<String>(n);
         numOfEdges=0;
+        isVisited=new boolean[5];
     }
+    //得到第一个邻接节点的下标w ,如果不存在返回-1
+    public int getFirstNeighbor(int index){
+        for (int j=0;j<vertexList.size();j++){
+            if (edges[index][j]>0){
+                return j;
+            }
+        }
+        return -1;
+    }
+    //根据前一个邻接节点的下标来获取下一个邻接节点
+    public int getNextNeighbor(int v1,int v2){
+        for (int j=v2+1;j<vertexList.size();j++){
+            if (edges[v1][j]>0){
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    //深度优先遍历算法
+    // i第一次是0
+    public void dfs(boolean[] isVisited,int i){
+        //首先我们访问该节点，输出
+        System.out.print(getValueByIndex(i) + "->");
+        //将节点设置为已访问
+        isVisited[i]=true;
+        //查找节点i的第一个邻接节点w
+        int w=getFirstNeighbor(i);
+        while (w!=-1){//说明有
+            if (!isVisited[w]){
+                dfs(isVisited,w);
+            }else {
+                //已经被访问过
+                w=getNextNeighbor(i,w);//找到下一个节点
+            }
+        }
+    }
+    //对dfs进行一个重载，遍历我们所有的节点，并进行dfs
+    public void dfs(){
+        //遍历所有的节点,进行dfs回溯
+        for (int i=0;i<getNumOfVertex();i++){
+            if (!isVisited[i]){
+                dfs(isVisited,i);
+            }
+        }
+    }
+
+
     //插入结点
     public void insertVertex(String vertex){
         vertexList.add(vertex);
